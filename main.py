e7f55935a69a294d4a080de646ec6f4a6ec809d8
RJMakarenko <propusk2@mail.ru>
2022-11-21 15:42:26 +0300

    Классное задание 1

--- main.py
+++ main.py
@@ -1,1 +1,39 @@
-print('Git')
+import random
+import sys
+
+from PyQt5.QtGui import QColor, QPainter
+from PyQt5.QtWidgets import QApplication, QMainWindow
+from PyQt5 import uic
+
+
+class Window(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        uic.loadUi('UI.ui', self)
+        self.flag = False
+        self.pushButton.clicked.connect(self.result)
+
+    def paintEvent(self, event):
+        if self.flag:
+            self.qp = QPainter()
+            self.qp.begin(self)
+            self.draw()
+            self.qp.end()
+
+    def draw(self):
+        self.colors = [255, 255, 0]
+        size = random.randrange(150)
+        self.qp.setBrush(QColor(*self.colors))
+        self.coords = [random.randrange(10, 200), random.randrange(80, 400)]
+        self.qp.drawEllipse(*self.coords, size, size)
+
+    def result(self):
+        self.flag = True
+        self.update()
+
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    ex = Window()
+    ex.show()
+    sys.exit(app.exec())
